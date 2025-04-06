5cebe7678e15067731e13edfacb8100aa67d3bd3
emidev98 <eharastasan@gmail.com>
2025-04-06 10:07:00 +0200

    feat: vercel speed insights

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
@@ -9,6 +10,7 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
+      <SpeedInsights />
     </BrowserRouter>
   </React.StrictMode>
 );
