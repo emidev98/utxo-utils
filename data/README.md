47c07c6385fcf57fae5e6ffe8b5dfad67eb7b83b
emidev98 <eharastasan@gmail.com>
2025-04-14 18:46:17 +0200

    feat: extend pricing data

--- data/README.md
+++ data/README.md
@@ -1,7 +1,7 @@
 # Data
 
-For the seek of simplicity and avoiding to pay an API for historical data I decided to use the dataset provided by [Novandra Anugrah in Kaggle](https://www.kaggle.com/datasets/novandraanugrah/bitcoin-historical-datasets-2018-2024/data) using the Avg price of a day and the date as a reference point.
+For the seek of simplicity and avoiding to pay an API for historical prices I decided to use the datasets provided by [Novandra Anugrah](https://www.kaggle.com/datasets/novandraanugrah/bitcoin-historical-datasets-2018-2024/data) and [SRK](https://www.kaggle.com/datasets/sudalairajkumar/cryptocurrencypricehistory?select=coin_Bitcoin.csv) in Kaggle, using the Avg price of a day and the date as a reference point.
 
-Since I also want to avoid creating an API, the data is compressed in the following format `TTTTMMDDNUMBERDECIMAL,` and appended one after the other `201801011328427,201801021418176,201801031472878...` to decreased the size of the file 370_623 bytes from to 41_713 bytes.
+Since I also want to avoid creating an API, the data is compressed in the following format `TTTTMMDDNUMBERDECIMAL,` and appended one after the other `201801011328427,201801021418176,201801031472878...` to decreased the size of the file 370_623 bytes from to 65_960 bytes.
 
 This is based on the assumption that it is easier to comput the data; instead of sending it over the network (PD: I hate apps that takes forever to load).
--- data/data-parser.ts
+++ data/data-parser.ts
@@ -3,27 +3,46 @@ import csvParser from "csv-parser";
 import dayjs from "dayjs";
 
 interface Data {
-  Open: number;
-  High: number;
+  Date: string;
   Low: number;
-  Close: number;
-  Volume: number;
-  CloseTime: string;
+  High: number;
 }
 
-const parseCsv = (filePath: string): Promise<Data[]> => {
+const parseDataUntil2018 = (): Promise<Data[]> => {
+  const OLD_DATA = "btc_1d_data_2013_to_2018.csv";
+
   return new Promise((resolve, reject) => {
     const data: Data[] = [];
-    fs.createReadStream(filePath)
+    fs.createReadStream(OLD_DATA)
       .pipe(csvParser())
       .on("data", (row) => {
         data.push({
-          Open: parseFloat(row.Open),
+          Date: row.Date,
           High: parseFloat(row.High),
           Low: parseFloat(row.Low),
-          Close: parseFloat(row.Close),
-          Volume: parseFloat(row.Volume),
-          CloseTime: row["Close time"],
+        });
+      })
+      .on("end", () => {
+        resolve(data);
+      })
+      .on("error", (err) => {
+        reject(err);
+      });
+  });
+};
+
+const parseDataFrom2018 = (): Promise<Data[]> => {
+  const NOT_SO_OLD_DATA = "btc_1d_data_2018_to_2025.csv";
+
+  return new Promise((resolve, reject) => {
+    const data: Data[] = [];
+    fs.createReadStream(NOT_SO_OLD_DATA)
+      .pipe(csvParser())
+      .on("data", (row) => {
+        data.push({
+          High: parseFloat(row.High),
+          Low: parseFloat(row.Low),
+          Date: row["Close time"],
         });
       })
       .on("end", () => {
@@ -38,7 +57,7 @@ const parseCsv = (filePath: string): Promise<Data[]> => {
 const storeCompresed = async (data: Data[]) => {
   const parsedDataset: { OpenTime: string; AvgHL: string }[] = data.map(
     (item) => ({
-      OpenTime: dayjs(item.CloseTime).format("YYYYMMDD"),
+      OpenTime: dayjs(item.Date).format("YYYYMMDD"),
       AvgHL: ((item.High + item.Low) / 2).toFixed(2).replace(".", ""),
     }),
   );
@@ -56,9 +75,11 @@ const storeCompresed = async (data: Data[]) => {
 
     buffer = Buffer.concat([buffer, entry]);
   }
-  fs.writeFileSync("btc-1-day-avg-historical.bin", buffer);
+  fs.writeFileSync("btc-1-day-avg-historical.txt", buffer);
 };
 
-parseCsv("btc_1d_data_2018_to_2025.csv")
-  .then(storeCompresed)
-  .catch((err) => console.error(err));
+(async () => {
+  const datUntil2018 = await parseDataUntil2018();
+  const dataFrom2018 = await parseDataFrom2018();
+  await storeCompresed([...datUntil2018, ...dataFrom2018]);
+})();
