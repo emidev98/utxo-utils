17a0281ad21e4a7372bd175b19b8f392846870bb
emidev98 <eharastasan@gmail.com>
2025-04-20 11:22:43 +0200

    feat: update libs and build prod and dev

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,30 @@
-// import legacy from '@vitejs/plugin-legacy'
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import viteCompression from "vite-plugin-compression";
 import Inspect from "vite-plugin-inspect";
 import { visualizer } from "rollup-plugin-visualizer";
+import VitePluginClean from "vite-plugin-clean";
+
+console.log("process.env.NODE_ENV", process.env.NODE_ENV);
+const isProd = process.env.NODE_ENV === "production";
+
+const prodPlugins = [
+  VitePluginClean(),
+  viteCompression({
+    verbose: true,
+  }),
+  viteCompression({
+    verbose: true,
+    algorithm: "brotliCompress",
+    compressionOptions: { level: 5 },
+  }),
+  visualizer({
+    open: true,
+    filename: "./dist/stats.html",
+  }),
+];
+
+const plugins = [react(), Inspect()];
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -21,6 +42,7 @@ export default defineConfig({
         manualChunks: {
           react: ["react", "react-dom"],
           lodash: ["lodash"],
+          apexcharts: ["apexcharts"],
         },
       },
     },
@@ -32,17 +54,5 @@ export default defineConfig({
       },
     },
   },
-  plugins: [
-    react(),
-    viteCompression({
-      verbose: true,
-    }),
-    viteCompression({ verbose: true, algorithm: "brotliCompress" }), // brotli
-    Inspect(),
-    visualizer({
-      open: true,
-      filename: "./dist/stats.html",
-    }),
-    // legacy()
-  ],
+  plugins: isProd ? [...plugins, ...prodPlugins] : plugins,
 });
