a0a97c5ad591b27a29414f35fa5c27456362ec85
emidev98 <eharastasan@gmail.com>
2025-04-20 12:16:01 +0200

    feat: under construction

--- src/pages/transactions/TransactionsPage.tsx
+++ src/pages/transactions/TransactionsPage.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import "./TransactionsPage.scss";
+import underConstruction from "/under-construction.png";
 
 const TransactionsPage: React.FC = () => {
-  return <div className="TransactionsPage">TransactionsPage</div>;
+  return (
+    <div className="TransactionsPage">
+      <img src={underConstruction} />
+    </div>
+  );
 };
 
 export default TransactionsPage;
